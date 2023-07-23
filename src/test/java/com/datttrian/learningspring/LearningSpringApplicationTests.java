4bb52afe01f01b1c1f4cfe689393b74fd713e53b
Dat T. Tran <datttrian@gmail.com>
2023-07-23 10:28:02 +0000

    Initiate spring project

---
+++ src/test/java/com/datttrian/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datttrian.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
